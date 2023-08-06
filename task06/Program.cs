02b48fa80d28083f85456b41f5c7214907354d67
ulaev <ulaev1990@mail.ru>
2023-08-06 11:51:54 +0300

    Added task06

---
+++ task06/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+int Even(int num)
+{
+    return num % 2;
+} 
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int even = Even(num);
+// if (even == 0)
+// {
+// Console.WriteLine("четное");
+// }
+// else 
+// {
+// Console.WriteLine("не четное");
+// }
+
+Console.WriteLine( even == 0? "Четное" : "Не четное");
